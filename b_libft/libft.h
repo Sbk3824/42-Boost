860fd2b31638559abf28509c1cc716da20249641
42final <skuntoji@e1z2r5p36.42.us.org>
2018-10-09 15:58:27 -0700

    code

---
+++ b_libft/libft.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skuntoji <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/12 08:41:35 by skuntoji          #+#    #+#             */
+/*   Updated: 2018/09/13 00:37:02 by skuntoji         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
+int					ft_toupper(int c);
+int					ft_tolower(int c);
+int					ft_isdigit(int c);
+
+void				*ft_memset(void *b, int c, size_t len);
+size_t				ft_strlen(const char *s);
+char				*ft_strdup(const char *s1);
+char				*ft_strcpy(char *dst, const char *src);
+char				*ft_strncpy(char *dst, const char *src, size_t len);
+char				*ft_strchr(const char *s, int c);
+int					ft_strcmp(const char *s1, const char *s2);
+
+#endif
---
+++ b_printf/b_printf.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   b_printf.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skuntoji <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/08 00:02:05 by skuntoji          #+#    #+#             */
+/*   Updated: 2018/10/08 00:02:08 by skuntoji         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "b_printf.h"
+
+void	format(va_list arg, char c, int *lenptr)
+{
+	if (c == 'c')
+		ft_printchar(va_arg(arg, int), lenptr);
+	else if (c == 's')
+		ft_printstr(va_arg(arg, char *), lenptr);
+	else if (c == 'd' || c == 'i')
+		ft_printint(va_arg(arg, int), lenptr);
+	else if (c == 'u')
+		ultoa_base(va_arg(arg, unsigned int), 10, lenptr);
+	else if (c == 'p')
+	{
+		ft_printstr("0x", lenptr);
+		ultoa_base(va_arg(arg, unsigned long), 16, lenptr);
+	}
+	else if (c == 'o')
+		ultoa_base(va_arg(arg, unsigned int), 8, lenptr);
+	else if (c == 'x')
+		ultoa_base(va_arg(arg, unsigned int), 16, lenptr);
+	else if (c == '%')
+		ft_printchar((int)'%', lenptr);
+}
+
+int		b_printf(const char *s, ...)
+{
+	int		len;
+	int		*lenptr;
+	va_list	arg;
+	int		i;
+
+	i = 0;
+	len = 0;
+	lenptr = &len;
+	va_start(arg, s);
+	while (s[i])
+	{
+		if (s[i] != '%')
+			ft_printchar((int)s[i], lenptr);
+		else
+		{
+			i++;
+			format(arg, s[i], lenptr);
+		}
+		i++;
+	}
+	va_end(arg);
+	return (len);
+}
